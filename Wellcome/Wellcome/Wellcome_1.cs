9c161b7ecb8307595f0b01974316883121e4e966
Angel3000 <adabasdeveloper@gmail.com>
2019-08-30 13:33:03 -0400

    Wellcome Screen
    Wellcome Screen

---
+++ Wellcome/Wellcome/Wellcome_1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.BackgroundColor = ConsoleColor.Cyan;
+        Console.ForegroundColor = ConsoleColor.DarkBlue;
+        Console.SetWindowSize(100, 10);
+        Console.Title = "Wellcome!!!!!";
+        Console.WriteLine("+-----------------------------------------+");
+        Console.WriteLine("|    Welcome to Acme Accounting Systems   |");
+        Console.WriteLine("|   Remember, we’re “accounting” on you!  |");
+        Console.WriteLine("+-----------------------------------------+");
+        Console.ReadLine();
+    }
+}
