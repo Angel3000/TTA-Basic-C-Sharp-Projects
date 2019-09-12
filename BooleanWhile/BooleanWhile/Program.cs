fd66d2a8038eb905ae7fd519ecda5e19d4c32553
Angel3000 <adabasdeveloper@gmail.com>
2019-09-12 11:38:32 -0400

    Boolean and While
    Boolean and While

---
+++ BooleanWhile/BooleanWhile/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace BooleanWhile
+{
+    class Program
+    {
+        static void Main()
+        {
+            string Scolor;
+            int Inum = 0;
+            bool Bcolor = false;
+            //
+            Console.WriteLine("Type the color:");
+            Scolor = Console.ReadLine();
+            Bcolor = Scolor == "Red";
+            while (!Bcolor)
+            {
+                Inum++; 
+                Console.WriteLine("Attempt.:" + Inum + "\nType the color:");
+                Scolor = Console.ReadLine();
+                Bcolor = Scolor == "Red";
+            }
+            Console.WriteLine("Congratulations!....Red is the color.");
+            Console.ReadLine();
+            //
+            //
+            //
+            do
+            {
+                Inum++;
+                Console.WriteLine("Attempt.:" + Inum + "\nType the color:");
+                Scolor = Console.ReadLine();
+                Bcolor = Scolor == "Blue";
+            }
+            while (!Bcolor);
+            Console.WriteLine("Congratulations!....Blue is the color.");
+            Console.ReadLine();
+        }
+
+    }
+}
