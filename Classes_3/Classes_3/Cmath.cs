2772c714eae5a26f12f2b15b24af875e5d241151
Angel3000 <adabasdeveloper@gmail.com>
2019-09-17 18:25:41 -0400

    Classes 3
    Classes 3

---
+++ Classes_3/Classes_3/Cmath.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_3
+{
+    public class Cmath
+    {
+        public int Dmath(int val1) 
+        {
+            int val2 = 0;
+            val2 = val1 * 1000;
+            return val2;
+        }
+        public int Dmath(decimal val1)
+        {
+            int val2 = 0;
+            val2 = Convert.ToInt32(val1);
+            val2 = val2 * 2;
+            return val2;
+        }
+        public int Dmath(string val1)
+        {
+            try
+            {
+                int val2 = 0;
+                val2 = Convert.ToInt32(val1);
+                val2 = val2 * 4;
+                return val2;
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Please send just numbers.");
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return 0;
+            }
+        }
+    }
+}
---
+++ Classes_3/Classes_3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_3
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n1 = 12;
+            decimal n2 = 12.3m;
+            string S1= "15";
+            Cmath cmath = new Cmath();
+            n1 = cmath.Dmath(12);
+            Console.WriteLine("The result of first method is: " + n1);
+            Console.ReadLine();
+            //
+            n1 = cmath.Dmath(n2);
+            Console.WriteLine("The result of second method is: " + n1);
+            Console.ReadLine();
+            //
+            n1 = cmath.Dmath(S1);
+            Console.WriteLine("The result of second method is: " + n1);
+            Console.ReadLine();
+            //
+        }
+    }
+}
