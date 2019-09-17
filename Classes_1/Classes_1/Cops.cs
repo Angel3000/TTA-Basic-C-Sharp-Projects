092c297f77349e8a9e132f42fb65f22cac288555
Angel3000 <adabasdeveloper@gmail.com>
2019-09-17 17:23:40 -0400

    Classes_1
    Classes

---
+++ Classes_1/Classes_1/Cops.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_1
+{
+    public class Cops
+    {
+        public static int DoSum(int val)
+        {
+            int outp = val + 23;
+            return outp;
+        }
+        //
+        public static int DoMul(int val)
+        {
+            int outp = val * 3;
+            return outp;
+        }
+        //
+        public static int DoAll(int val)
+        {
+            int outp = val * 3  + 23;
+            return outp;
+        }
+    }
+}
---
+++ Classes_1/Classes_1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_1
+{
+    class Program
+    {
+        static void Main()
+        {
+            int In1, In2;
+            string Sn1;
+            bool Bval;
+            do
+            {
+                Console.WriteLine("Please type a number:");
+                Sn1 = Console.ReadLine();
+                Bval = false;
+                Bval = int.TryParse(Sn1, out In1);
+            } while (!Bval);
+            //
+            In2 = Cops.DoSum(In1);
+            Console.WriteLine("First operation: " + In2);
+            In2 = Cops.DoMul(In1);
+            Console.WriteLine("Second operation: " + In2);
+            In2 = Cops.DoAll(In1);
+            Console.WriteLine("Third operation: " + In2);
+            Console.ReadLine();
+            }
+    }
+}
