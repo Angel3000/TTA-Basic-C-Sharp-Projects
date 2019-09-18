b8a8aa84a908926b9160aa2e223b73a7c1cd3807
Angel3000 <adabasdeveloper@gmail.com>
2019-09-18 11:49:15 -0400

    Classes_6
    Classes task

---
+++ Classes_6/Classes_6/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_6
+{
+    class Program
+    {
+        static void Main()
+        {
+            Employee employee = new Employee();
+            employee.FirstName = "Sample";
+            employee.LastName = "Student";
+            employee.SayName();
+            Console.ReadLine();
+        }
+    }
+}
