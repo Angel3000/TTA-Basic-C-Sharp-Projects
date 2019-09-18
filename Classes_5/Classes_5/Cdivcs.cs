f3c33654aef8d748bf8c5f867b5976efeef2f1db
Angel3000 <adabasdeveloper@gmail.com>
2019-09-18 10:33:44 -0400

    Classes_5
    Classes 5

---
+++ Classes_5/Classes_5/Cdivcs.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_5
+{
+    public static class Cdivcs
+    {
+        public static int Cdivs(int val1)
+        {
+            int val2 = val1 / 2;
+            return val2;
+        }
+        //
+        public static void Cdivs(int val1, out int val2)
+        {
+            val2 = val1 / 2;
+        }
+    }
+}
---
+++ Classes_5/Classes_5/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_5
+{
+    class Program
+    {
+        static void Main()
+        {
+            bool Bval;
+            string Sn1;
+            int In1, In2, val3;
+            //
+            do
+            {
+                Console.WriteLine("Please type a number:");
+                Sn1 = Console.ReadLine();
+                Bval = false;
+                Bval = int.TryParse(Sn1, out In1);
+            } while (!Bval);
+            //
+            In2 = Cdivcs.Cdivs(In1);
+            Console.WriteLine("First method, value: " + In2);
+            //
+            Cdivcs.Cdivs(In1, out val3);
+            Console.WriteLine("Second method, value: " + val3);
+            //
+            Console.ReadLine();
+        }
+    }
+}
