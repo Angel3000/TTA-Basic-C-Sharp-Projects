59ea945a1fcfe2cd77b6de23bf2cf9a16fd1ed9e
Angel3000 <adabasdeveloper@gmail.com>
2019-09-17 20:09:37 -0400

    Classes_4
    4t Classes task

---
+++ Classes_4/Classes_4/Cmath.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_4
+{
+    public class Cmath
+    {
+        public static int cmath(int val1, int val2 = 0)
+        {
+            int val3;
+            val3 = val1 + 50;
+            if (!(val2 == 0))
+            {
+                val3 = val3 - val2;
+            }
+            return val3;
+        }
+    }
+}
---
+++ Classes_4/Classes_4/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_4
+{
+    class Program
+    {
+        static void Main()
+        {
+            bool Bval;
+            string Sn1;
+            int In1, In2, val3;
+            do
+            {
+                Console.WriteLine("Type the first number:");
+                Sn1 = Console.ReadLine();
+                Bval = false;
+                Bval = int.TryParse(Sn1, out In1);
+                
+            } while (!Bval);
+            do
+            {
+                Sn1 = " ";
+                Console.WriteLine("Type the second number. This number is optional:");
+                Sn1 = Console.ReadLine(); 
+                Bval = false;
+                if (string.IsNullOrEmpty(Sn1))
+                {
+                    Bval = true;
+                    In2 = 0;
+                }
+                else
+                {
+                    Bval = int.TryParse(Sn1, out In2);
+                }
+            } while (!Bval);
+            Cmath cmath = new Cmath();
+            val3 = Cmath.cmath(In1);
+            Console.WriteLine("Only with one parameter. Result: " + val3);
+            //
+            val3 = Cmath.cmath(In1, In2);
+            Console.WriteLine("With two parameters. Result: " + val3);
+            Console.ReadLine();
+        }
+    }
+}
