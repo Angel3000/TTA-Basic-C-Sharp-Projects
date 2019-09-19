aa4ac34a2a2b5cba506b3cedf58c5480543728bc
Angel3000 <adabasdeveloper@gmail.com>
2019-09-19 12:02:22 -0400

    Struct_1
    Struct task

---
+++ Struct_1/Struct_1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct_1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Number num = new Number();
+            num.Amount = 100.23m;
+            Console.WriteLine("Amount is: " + num.Amount);
+            Console.ReadLine();
+        }
+        public struct Number
+        {
+            public decimal Amount { get; set; }
+        }
+    }
+}
