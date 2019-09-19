a58e4da8e3796e2445889fcb4fa6b9fce258408f
Angel3000 <adabasdeveloper@gmail.com>
2019-09-19 11:40:45 -0400

    Enum_1
    Enum oarse

---
+++ Enum_1/Enum_1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enum_1
+{
+    class Program
+    {
+        static void Main()
+        {
+            string Sday;
+            try
+            {
+                Console.WriteLine("Please type the current day");
+                Sday = Console.ReadLine().ToLower();
+                WeekDays wday = (WeekDays)Enum.Parse(typeof(WeekDays), Sday);
+                Console.WriteLine("That's a day of the week");
+                         }
+            catch (System.ArgumentException ex)
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+                
+        }
+        public enum WeekDays
+        {
+            sunday,
+            monday,
+            tuesday,
+            wednesday,
+            thursday,
+            friday,
+            saturday
+        }
+    }
+}
--- TwentyOne/TwentyOne/Deck.cs
+++ TwentyOne/TwentyOne/Deck.cs
@@ -11,18 +11,13 @@ namespace TwentyOne
         public Deck()
         {
             Cards = new List<Card>();
-            List<string> Suits = new List<string>() { "Clubs", "Hearts", "Spades", "Diamonds" };
-            List<string> Faces = new List<string>()
-            {   "Two", "Three", "Four", "Five", "Six" , "Seven", "Eigth",
-                "Nine", "Ten", "Jack", "Queen", "King", "Ace" };
-            //
-            foreach (string face in Faces)
-            { 
-                foreach (string suit in Suits)
+            for (int i = 0; i < 13; i++ )
+            {
+                for (int j = 0; j < 4; j++)
                 {
                     Card card = new Card();
-                    card.Suit = suit;
-                    card.Face = face;
+                    card.Face = (Face)i;
+                    card.Suit = (Suit)j;
                     Cards.Add(card);
                 }
             }
