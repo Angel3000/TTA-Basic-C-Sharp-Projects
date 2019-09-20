e8089094f72bd562e28f4526b774513a8b6dde17
Angel3000 <adabasdeveloper@gmail.com>
2019-09-19 22:13:00 -0400

    Files_1
    Files task

---
+++ Files_1/Files_1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Files_1
+{
+    class Program
+    {
+        static void Main()
+        {
+            string anwser;
+            do
+            {
+                Console.WriteLine("Please enter a number:");
+                string num = Console.ReadLine();
+                //
+                using (StreamWriter file = new StreamWriter(@"C:\Users\angel.lopez\Desktop\VSP\TTA-Basic-C-Sharp-Projects\Files_1\log.txt", true))
+                {
+                    file.WriteLine(num);
+                }
+                Console.WriteLine("Number added to file. Do you want to add more?(Y/N)");
+                anwser = Console.ReadLine().ToUpper();
+            } while (anwser == "Y");
+            //
+            string[] lines = File.ReadAllLines(@"C:\Users\angel.lopez\Desktop\VSP\TTA-Basic-C-Sharp-Projects\Files_1\log.txt");
+            Console.WriteLine("\n\nThis is what you typed:");
+            foreach(string line in lines)
+            {
+                Console.WriteLine(line);
+            }
+            Console.ReadLine();
+        }
+    }
+}
--- TwentyOne/TwentyOne/Program.cs
+++ TwentyOne/TwentyOne/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace TwentyOne
 {
@@ -10,15 +11,36 @@ namespace TwentyOne
     {
         static void Main(string[] args)
         {
-            
-            Deck deck = new Deck();
-            deck.Shuffle(3);
+            //string text = "Here is a text line";
+            //File.WriteAllText(@"C:\Users\angel.lopez\Desktop\VSP\TTA-Basic-C-Sharp-Projects\TwentyOne\log.txt", text);
             //
-            foreach (Card card in deck.Cards)
+            //string text = File.ReadAllText(@"C:\Users\angel.lopez\Desktop\VSP\TTA-Basic-C-Sharp-Projects\TwentyOne\log.txt");
+            //Console.WriteLine(text);
+            //
+            Console.WriteLine("Wellcome to the casino. Tell me your name:");
+            string playerName = Console.ReadLine();
+
+            Console.WriteLine("How much money do you bring?");
+            int bank = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Hello {0}, Would you like to join blackjack?", playerName);
+            string answer = Console.ReadLine().ToLower();
+
+            if (answer == "yes" || answer == "yeah" || answer == "y" || answer == "ya")
             {
-                Console.WriteLine(card.Face + " of " + card.Suit);
+                Player player = new Player(playerName, bank);
+                Game game = new TwentyOneGame();
+                game += player;
+                player.isActivelyPlaying = true;
+                while (player.isActivelyPlaying && player.Balance > 0)
+                {
+                    game.Play();
+                }
+                game -= player;
+                Console.WriteLine("Thanks for playing.");
             }
-            Console.WriteLine(deck.Cards.Count);
+
+            Console.WriteLine("Feel fre to look arround");
             Console.ReadLine();
         }
     }
